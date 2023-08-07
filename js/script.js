a24cc6742436fe9daa7c37ed742a0e68724abd7d
preich-work <preich_work@yahoo.com>
2023-08-06 20:54:18 -0400

    changed JS for button and picture

--- js/script.js
+++ js/script.js
@@ -2,7 +2,17 @@ var button = document.querySelector(".show-cat");
 //console.log(button);
 var cat = document.querySelector(".cat");
 //console.log(cat);
-button.addEventListener("click", function () {
+button.addEventListener("click", function () 
+{
   //console.log("It's working!");
-  cat.classList.add("show");
-});
+  if (cat.classList.contains("show"))
+  { cat.classList.remove("show");
+button.innerText = "Wait! Come back!";
+button.classList.add("disappear");}
+  //console.log("Yes");}
+
+  else {//console.log("No");
+    cat.classList.add("show");
+    button.innerText = "Shoo, cat!";
+button.classList.remove("disappear");
+}});
